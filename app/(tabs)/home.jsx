b440518aafde9d73e9fe7733c3de039acb1d67b6
Marion Poljakov <marion.poljakov@gmail.com>
2024-09-27 10:35:13 +0300

    Profile screen / logout function

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -8,8 +8,10 @@ import  EmptyState  from "@/components/EmptyState"
 import  SearchInput  from '@/components/SearchInput'
 import  VideoCard  from '@/components/videoCard'
 import  Trending  from '@/components/Trending'
+import { useGlobalContext } from '@/context/GlobalProvider'
 
 const Home = () => {
+  const { user, setUser, setIsLoggedIn } = useGlobalContext();
   const { data: posts, refetch} = useAppwrite(getAllPosts);
   const { data: latestPosts} = useAppwrite(getLatestPosts);
 
@@ -34,8 +36,8 @@ const Home = () => {
           <View className="my-6 px-4 space-y-6">
             <View className="justify-between items-start flex-row mb-6">
               <View>
-                <Text className="font-pmedium text-sm text-gray-100">Welcome back</Text>
-                <Text className="text-2xl font-psemibold text-white">Mari</Text>
+                <Text className="font-pmedium text-sm text-gray-100">Welcome back,</Text>
+                <Text className="text-2xl font-psemibold text-white">{user?.username}</Text>
               </View>
               <View className="mt-1.5">
                 <Image
